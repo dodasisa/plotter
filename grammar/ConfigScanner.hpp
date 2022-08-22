fd6303b777c44c59e6576b2040a8e0fb2ffe8cdc
dodasisa <david.lopez.velazquez@gmail.com>
2022-08-22 16:33:18 +0200

    lexer evolving. Not good yet

--- grammar/ConfigScanner.hpp
+++ grammar/ConfigScanner.hpp
@@ -5,6 +5,6 @@ namespace CP
 		public:
 			CP_Scanner(std::istream& arg_yyin, std::ostream& arg_yyout): yyFlexLexer(arg_yyin, arg_yyout) {}
 			CP_Scanner(std::istream* arg_yyin = nullptr, std::ostream* arg_yyout = nullptr): yyFlexLexer(arg_yyin, arg_yyout) {}
-			int lex(Parser::semantic_type *yylval); // note: this is the prototype we need
+			int lex(CP_Parser::semantic_type *yylval); // note: this is the prototype we need
 	}
 }
